d31b0944d61ef725f8832585553d924a5e8f66bc
Abigail <abigail.mcphillips@gmail.com>
2016-08-10 17:33:13 +0100

    Adds message for guess that are too low, and correct guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,6 +6,10 @@ number = rand(101)
 get '/' do
   if params['guess'].to_i > number
     message = 'Too high!'
+  elsif params['guess'].to_i < number
+    message = 'To low!'
+  else
+    message = 'You got it right!'
   end
   erb :index, :locals => {:number => number, :message => message}
 
